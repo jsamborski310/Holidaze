a69a52330e39e282904d05553fbf2c4874397aca
Ethan Buchanan <ethan@buchananville.com>
2021-12-11 13:01:46 -0800

    Created basic structure and connected to form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,60 @@ document.addEventListener('DOMContentLoaded', function() {
   //   var elems = document.querySelectorAll('.datepicker');
   //   var instances = M.Datepicker.init(elems, options);
   // });
+
+var thisYearData;
+var nextYearData;
+
+function fetchFilteredHolidays(event)
+{
+  // stops page reload on submit
+  event.preventDefault();
+
+  // initialize thisYearData and nextYearData
+  thisYearData = false;
+  nextYearData = false;
+
+  // find this year
+  var thisYear = parseInt( moment().format("yyyy"));
+
+  // get this year
+  fetch("https://calendarific.com/api/v2/holidays?&api_key=" + apiKey + "&country=US&year="+thisYear)
+  .then(function (responce){
+    return responce.json();
+  })
+  .then(function (data)
+  {
+    thisYearData = data;
+
+    getFilteredHolidays();
+  })
+
+  // get next year
+  fetch("https://calendarific.com/api/v2/holidays?&api_key=" + apiKey + "&country=US&year="+(thisYear+1))
+  .then(function (responce){
+    return responce.json();
+  })
+  .then(function (data)
+  {
+    nextYearData = data;
+
+    getFilteredHolidays();
+  })
+}
+
+
+function getFilteredHolidays()
+{
+  if (! thisYearData || !nextYearData)
+  {
+    return;
+  }
+
+
+
+
+
+  console.log("getFilteredHolidays got called");
+}
+
+document.querySelector("form").addEventListener("submit",fetchFilteredHolidays)

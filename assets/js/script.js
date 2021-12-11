a449d04ce029fac275fa8e049ce76266a452f23b
Nick Edwards <theadventuresofnickedwards@gmail.com>
2021-12-11 12:06:08 -0800

    Created getHolidays function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,50 @@ document.addEventListener('DOMContentLoaded', function() {
   //   var instances = M.FormSelect.init(elems, options);
   // });
 
+var searchInput = 'Arbor Day'
+
+function getHolidays() {
+  var getHolidaysURL = `https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=US&year=2021`
+  var searchedHolDate;
+  var searchedHolName;
+  var searchedHolDescription;
+  var searchedHolCountry;
+  var searchedHolType;
+
+  // Fetch request for holiday data (default: country = US, year = current year)
+  fetch(getHolidaysURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("data from getHolidays is:");
+    console.log(data);
+
+    // Iterate to find holiday name that matches
+    for (searchedHolidayData of data.response.holidays) {
+      if (searchInput !== searchedHolidayData.name) continue;
+      console.log("data from searchedHolidayData is:")
+      console.log(searchedHolidayData)
+
+      // Set variables to pass to displayHolidays
+      searchedHolDate = searchedHolidayData.date.iso;
+      searchedHolName = searchedHolidayData.name;
+      searchedHolDescription = searchedHolidayData.description;
+      searchedHolCountry = searchedHolidayData.country.name;
+      searchedHolType = searchedHolidayData.type[0];
+
+      console.log("All variables passing from getHolidays to displayHolidays:")
+      console.log(searchedHolDate, searchedHolName, searchedHolDescription, searchedHolCountry, searchedHolType)
+      
+      // displayHolidays(searchedHolDate, searchedHolName, searchedHolDescription, searchedHolCountry, searchedHolType);
+
+    }
+  })
+}
+
+getHolidays();
+
+
 
 
   
